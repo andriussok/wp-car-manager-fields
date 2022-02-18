6d641047d3cf7761920339009efda2152716e43c
andriussok <40456865+andriussok@users.noreply.github.com>
2022-02-18 22:19:55 +0000

    header fn removed, no need for ABSPATH

--- wp-car-manager-fields.php
+++ wp-car-manager-fields.php
@@ -1,18 +1,18 @@
 <?php
 /*
     Plugin Name: WP Car Manager Fields
-		Plugin URI: https://gloss.agency
+    Plugin URI: https://github.com/andriussok/wp-car-manager-fields
     Description: Add new make-model helper. Plugin adds make-model metabox in single car admin and allows to add new make-model in frontend car-submission form.
     Version: 1.0.0
-    Author: AndriusSok
+    Author: Andrius Sok.
     Author URI: https://github.com/andriussok
     License: GPL v2
 */
 
 // Exit if accessed directly
 if ( ! defined( 'ABSPATH' ) ) {
-    header( 'Status: 403 Forbidden', true, 403 );
-	exit;
+    // header( 'Status: 403 Forbidden', true, 403 );
+    exit; // Exit if accessed directly
 }
 
 // Check dependencies
